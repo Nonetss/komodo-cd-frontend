8e21a94171b62f8d94c3cfe49ff3290481c08d71
Nonete <amorenolopezbarajas@pm.me>
2026-01-08 22:53:11 +0100

    Update dependencies, format files, add husky pre-commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
